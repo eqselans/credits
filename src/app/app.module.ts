4af9990b493649ff6411ff57a6ac545b3fc91e6f
Emirhan AKSU <emirhanaksu1903@gmail.com>
2023-06-20 16:30:26 +0300

    Fonksiyonlar oluşturuldu ve değişiklikler yapıldı.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { CreditsComponent } from './credits/credits.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CreditsComponent } from './credits/credits.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/credits/credits.component.ts
+++ src/app/credits/credits.component.ts
@@ -6,5 +6,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./credits.component.css']
 })
 export class CreditsComponent {
+  cards: { kredi: number|null, not: string }[] = [];
+  gano: number | null = null;
 
+
+
+  addCard() {
+    this.cards.push({ kredi: null, not: '' });
+  }
+
+  handleChange(value: string, index: number): void {
+    this.cards[index].not = value;
+  }
+
+  hesapla() {
+    let toplamKredi=0;
+    let toplamAno = 0;
+
+    for(const element of this.cards){
+      const card = element
+      console.log(toplamKredi);
+
+      if (card.kredi !== null || card.kredi !=='' && card.not !== 'Seçiniz') {
+        const kredi = Number(card.kredi);
+        const not = card.not
+        console.log(not)
+
+        toplamKredi += kredi;
+        console.log(toplamKredi);
+  
+        console.log(toplamAno);
+        toplamAno += kredi * this.harfNotuCevir(card.not);
+        console.log(this.harfNotuCevir(card.not))
+        console.log(toplamAno);
+      }
+      
+    }
+    this.gano = toplamKredi > 0 ? toplamAno / toplamKredi : null;
+    console.log(this.gano);
+
+    
+  }
+
+  harfNotuCevir(harfNotu:string):number{
+    switch (harfNotu) {
+      case "AA":
+        return 4.0;
+      case 'BA':
+        return 3.5;
+      case 'BB':
+        return 3.0;
+      case 'CB':
+        return 2.5;
+      case 'CC':
+        return 2.0;
+      case 'DC':
+        return 1.5;
+      case 'DD':
+        return 1.0;
+      default:
+        return 0.0;
+    }
+  }
 }
